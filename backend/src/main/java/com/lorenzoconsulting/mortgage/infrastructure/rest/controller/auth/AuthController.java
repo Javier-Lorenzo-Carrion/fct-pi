fe3ef0a2e3958aa6d699eeab4f50a5229f04c37b
Javier Lorenzo Carrion <javierlorenzocarrion@gmail.com>
2025-05-22 17:37:57 +0100

    refactor: remove user controller and move signup to auth controller

--- backend/src/main/java/com/lorenzoconsulting/mortgage/infrastructure/rest/controller/auth/AuthController.java
+++ backend/src/main/java/com/lorenzoconsulting/mortgage/infrastructure/rest/controller/auth/AuthController.java
@@ -1,6 +1,9 @@
 package com.lorenzoconsulting.mortgage.infrastructure.rest.controller.auth;
 
 import com.lorenzoconsulting.mortgage.business.application.service.JwtService;
+import com.lorenzoconsulting.mortgage.business.application.service.UserService;
+import com.lorenzoconsulting.mortgage.infrastructure.rest.controller.user.CreateUserRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,10 +17,12 @@ public class AuthController {
 
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final UserService userService;
 
-    public AuthController(JwtService jwtService, AuthenticationManager authenticationManager) {
+    public AuthController(JwtService jwtService, AuthenticationManager authenticationManager, UserService userService) {
         this.jwtService = jwtService;
         this.authenticationManager = authenticationManager;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -30,5 +35,11 @@ public class AuthController {
         String jwt = jwtService.generateToken(authentication.getName()); // esto como si fuera el email
 
         return ResponseEntity.ok(new AuthResponse(jwt));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> create(@RequestBody CreateUserRequest createUserRequest) {
+        userService.create(createUserRequest.toFields());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
